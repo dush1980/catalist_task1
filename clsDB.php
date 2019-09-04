4c485be0442c38aee8900c2920dc91941712a3f1
Dushmantha <54741401+dush1980@users.noreply.github.com>
2019-09-04 21:19:29 +1000

    Database connection class
    database connection class basic structure

---
+++ clsDB.php
@@ -0,0 +1,44 @@
+<?
+// Singleton to connect db
+class clsDB {
+	// Hold the class instance.
+  	private static $db = null;
+  	private $conn;
+  
+  	private $error = false;
+  	private $msg = '';
+   
+  	// The db connection constructor
+  	private function __construct($host, $user, $pwd, $db) {
+    		$this->conn = new mysqli($host, $user, $pwd, $db);
+		if ($this->conn->connect_errno) {		
+			$this->msg="Could not connect to the database";
+			$this->error=true;
+		}
+  	}
+  	
+  	//create a new connection if already not created
+  	public static function createConn() {
+		if(!self::$db)  self::$db = new clsDB();
+		return self::$db;
+  	}
+  	
+  	//check if any error
+  	public function isError() { return $this->$error; } 
+  	
+  	//get the error message
+  	public function getMsg() { return $this->msg; }
+  
+  	//create table / empty table
+  	public function createTable($tbl) {
+  	
+  	}
+  	
+  	//insert the row
+  	//return : true on success, false on fail with error set
+  	//can be false if uniqe not matched
+  	public function insertRow($name, $surname, $email) {
+  	
+  	}
+}
+
